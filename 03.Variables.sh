b594dac35c639cd3b12568a271c443f0b92fe58e
satheesh37 <satheesh.batreddy1120@gmail.com>
2024-11-28 12:19:51 +0530

    02.Conversition.sh

---
+++ 03.Variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+Person1=$1
+Person2=$2
+
+echo "$Person1 :: Hi $Person2, How are you?"
+echo "$Person2 :: Hi $Person1, I am fine. How are you doing?"
+echo "$Person1 :: I am doing good $Person2. what's go in on"
+echo "$Person2 :: I am learning devops course $Person1"
