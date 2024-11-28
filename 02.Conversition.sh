d601c15b7e99293d49e19f23f21f3c2866e386d9
satheesh37 <satheesh.batreddy1120@gmail.com>
2024-11-28 12:15:10 +0530

    02.Conversition.sh

--- 02.Conversition.sh
+++ 02.Conversition.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-echo "Ramesh :: Hi Suresh, How are you?"
-echo "Suresh :: Hi Ramesh, I am fine. How are you doing?"
-echo "Ramesh :: I am doing good Suresh. what's go in on"
-echo "Suresh :: I am learning devops course Ramesh"
+Person1=Raheem
+Person2=Suresh
+
+echo "$Person1 :: Hi $Person2, How are you?"
+echo "$Person2 :: Hi $Person1, I am fine. How are you doing?"
+echo "$Person1 :: I am doing good $Person2. what's go in on"
+echo "$Person2 :: I am learning devops course $Person1"
