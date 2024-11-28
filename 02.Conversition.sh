3f73bdea1a1cd206697031fe96d20267d66a1002
satheesh37 <satheesh.batreddy1120@gmail.com>
2024-11-28 11:50:38 +0530

    added conversastion script

---
+++ 02.Conversition.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh :: Hi Suresh, How are you?"
+echo "Suresh :: Hi Ramesh, I am fine. How are you doing?"
+echo "Ramesh :: I am doing good Suresh. what's go in on"
+echo "Suresh :: I am learning devops course Ramesh"
