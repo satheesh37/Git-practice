d8d85c1b01a06358038744241a987fb8622959dc
satheesh37 <satheesh.batreddy1120@gmail.com>
2024-11-29 16:09:26 +0530

    functions

--- 11.Instalations.sh
+++ 11.Instalations.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-# USERID=$(id -u)
+USERID=$(id -u)
 
-# if[ USERID -ne 0 ]
-# then
-#   echo "Please run this script"
-#        #exit 1
-# fi
+if [ USERID -ne 0 ]
+then
+       echo "Please run this script"
+       #exit 1
+fi
 
-# dnf install git -y
+dnf install git -y
 
-ls -ls
 
-lsss -l
-
-ls -l
